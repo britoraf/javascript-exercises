de3bfcf302701785662ad8e4e6ac48ad022f75af
britoraf <rafaelbrito__@outlook.com>
2025-01-26 00:53:02 +0000

    Exercise 04 OK

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+  let result = array.filter(item => !arg.includes(item));
+  return result
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
