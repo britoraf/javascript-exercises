bfe199cb47206f8fa6a9ec4a7faf41d405d1a63b
britoraf <rafaelbrito__@outlook.com>
2025-01-27 00:01:15 +0000

    Exercises 5, 6 and 7 OK

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function (int1, int2) {
+  if (int1 > 0 && int2 > 0 && typeof int1 === "number" && typeof int2 === "number" && (int1 % 1 === 0) && (int2 % 1 === 0)) {
+    let numMin = Math.min(int1, int2);
+    let numMax = Math.max(int1, int2);
+    let sum = 0;
+    for (let i = numMin; i <= numMax; i++) {
+      sum += i;
+    }
+    return sum
+  }
+  else return "ERROR"
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let convertedC = (tempF - 32) * 5/9
+  return Number(convertedC.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convertedF = (tempC * 9/5) + 32
+  return Number(convertedF.toFixed(1))
 };
 
 // Do not edit below this line
