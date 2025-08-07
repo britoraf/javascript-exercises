05405aa3f78fd9121f148c89c4b31529bdf2fe65
britoraf <rafaelbrito__@outlook.com>
2025-08-07 21:50:11 +0100

    Exercise 12 at 60%

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...args) {
+  let numbers = args;
+  let calculus = numbers.reduce((acc, curr) => acc + curr, 0);
+  return calculus;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let numbers = arr;
+  return numbers.reduce((acc, curr) => acc + Number(curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let numbers = arr;
+  return numbers.reduce((acc, curr) => acc * Number(curr), 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
 const factorial = function() {
-	
+
 };
 
 // Do not edit below this line
