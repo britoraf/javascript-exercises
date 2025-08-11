aca870ef6334e4b3a98539a1dbb9af8e0eaa9a1b
britoraf <rafaelbrito__@outlook.com>
2025-08-11 23:58:39 +0100

    Exercises 12 and 13 done.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,32 +1,36 @@
-const add = function(...args) {
+const add = function (...args) {
   let numbers = args;
   let calculus = numbers.reduce((acc, curr) => acc + curr, 0);
   return calculus;
 };
 
-const subtract = function(num1, num2) {
+const subtract = function (num1, num2) {
   return num1 - num2;
 };
 
-const sum = function(arr) {
+const sum = function (arr) {
   let numbers = arr;
   return numbers.reduce((acc, curr) => acc + Number(curr), 0);
 };
 
-const multiply = function(arr) {
+const multiply = function (arr) {
   let numbers = arr;
   return numbers.reduce((acc, curr) => acc * Number(curr), 1);
 };
 
-const power = function(num1, num2) {
+const power = function (num1, num2) {
   return Math.pow(num1, num2);
 };
 
-const factorial = function(num) {
-    for (let i = 2; i <= num; i++) {
-      num *= i;
-  };
-
+const factorial = function (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -36,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringLowerCase = string.toLowerCase();
+    let noAlphaNumRemoval = stringLowerCase.replace(/[^a-z0-9]/g,"");
+    let stringArray = noAlphaNumRemoval.split("");
+    let stringReversed = [];
+    let isPalindrome = false;
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        stringReversed.push(stringArray[i]);
+    }
+
+    if (stringReversed.join("") === stringArray.join("")) {
+        isPalindrome = true;
+    } else {
+        return isPalindrome;
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
