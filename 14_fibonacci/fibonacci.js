07d0fca284568bf790bf283de5217d275661ccef
britoraf <rafaelbrito__@outlook.com>
2025-08-13 23:30:05 +0100

    Exercise 14 done.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (position) {
+  let fibonacciArray = [0, 1];
+  if (position >= 0) {
+    for (let i = 0; i <= 100; i++) {
+      fibonacciArray.push(fibonacciArray[i] + fibonacciArray[i + 1])
+    }
+  } else return "OOPS";
+ return fibonacciArray[position];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
